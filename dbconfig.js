beca68909045c81128c203f668a836bc20c0a286
Aakash3182 <Aakash3182>
2024-12-23 11:45:02 +0530

    updated

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    return await mongoose.connect(process.env.MONGO_DEV)
+   
+   if (process.env.MODE==="production"){
+    return await mongoose.connect(process.env.MONGO_CLOUD)
+   } else{ return await mongoose.connect(process.env.MONGO_DEV)}
 }
 
 module.exports = connectDB
